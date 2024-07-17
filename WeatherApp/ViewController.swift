4361edcc18df37121e86cc841bef335e5403646b
Morozov Artem <Vjhjpjaa32@yandex.ru>
2024-07-17 23:30:23 +0300

    add animation

--- WeatherApp/ViewController.swift
+++ WeatherApp/ViewController.swift
@@ -20,7 +20,8 @@ class ViewController: UIViewController {
     @IBOutlet weak var cloudsButton: UIButton!
     @IBOutlet weak var snowButton: UIButton!
     
-    private weak var imageView: UIImageView?
+    private weak var mainWeatherImageView: UIImageView?
+    private weak var snowflake: UIImageView?
     
     var buttons = [UIButton]()
     private var currentWeatherState: CurrentWeather = .sun {
@@ -28,7 +29,6 @@ class ViewController: UIViewController {
             updateUI()
         }
     }
-    
     
     
     
@@ -54,6 +54,9 @@ class ViewController: UIViewController {
     
     
     private func updateUI() {
+        mainWeatherImageView?.layer.removeAllAnimations()
+        snowflake?.layer.removeAllAnimations()
+        snowflake?.removeFromSuperview()
         resetSelectButtons()
         selectButton()
     }
@@ -68,36 +71,100 @@ class ViewController: UIViewController {
     private func selectButton() {
         switch currentWeatherState {
         case .sun:
+            self.mainWeatherImageView?.image = UIImage(named: "sunBig")
             sunButton.layer.cornerRadius = 15
             sunButton.clipsToBounds = true
             sunButton.backgroundColor = #colorLiteral(red: 0.2588235294, green: 0.2941176471, blue: 0.3294117647, alpha: 1)
+            animationSelector()
+            sunAnimation()
             
         case .rain:
+            self.mainWeatherImageView?.image = UIImage(named: "rainBig")
             rainButton.layer.cornerRadius = 15
             rainButton.clipsToBounds = true
             rainButton.backgroundColor = #colorLiteral(red: 0.2588235294, green: 0.2941176471, blue: 0.3294117647, alpha: 1)
+            animationSelector()
+            animateBackgroundRain()
         case .clouds:
+            self.mainWeatherImageView?.image = UIImage(named: "cloudsBig")
             cloudsButton.layer.cornerRadius = 15
             cloudsButton.clipsToBounds = true
             cloudsButton.backgroundColor = #colorLiteral(red: 0.2588235294, green: 0.2941176471, blue: 0.3294117647, alpha: 1)
+            animationSelector()
         case .snow:
+            self.mainWeatherImageView?.image = UIImage(named: "snowBig")
             snowButton.layer.cornerRadius = 15
             snowButton.clipsToBounds = true
             snowButton.backgroundColor = #colorLiteral(red: 0.2588235294, green: 0.2941176471, blue: 0.3294117647, alpha: 1)
+            animationSelector()
+            animateSnowFlake()
         }
     }
     
+    func animationSelector() {
+        UIView.animate(withDuration: 0.5, animations: {
+            self.mainWeatherImageView?.transform = CGAffineTransform(scaleX: 0.5, y: 0.5)
+        }) { _ in
+            UIView.animate(withDuration: 0.5) {
+                self.mainWeatherImageView?.transform = CGAffineTransform.identity
+            }
+        }
+    }
     
+    func animateBackgroundRain() {
+        UIView.animate(withDuration: 0.1, animations: {
+            self.view.backgroundColor = .white
+        }) { _ in
+            UIView.animate(withDuration: 0.1) {
+                self.view.backgroundColor = #colorLiteral(red: 0.1803921569, green: 0.5254901961, blue: 0.6705882353, alpha: 1)
+            }
+        }
+    }
     
+    private func animateSnowFlake() {
+        guard let mainWeatherImageView = mainWeatherImageView else {return}
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "snowflake")
+        imageView.frame = CGRect(origin: CGPoint(x: Int(mainWeatherImageView.frame.midX), y: Int(mainWeatherImageView.frame.maxY)), size: CGSize(width: 50, height: 50))
+        view.addSubview(imageView)
+        self.snowflake = imageView
+        let endPoint = CGPoint(x: imageView.frame.origin.x, y: view.frame.height)
+        
+        UIView.animate(withDuration: 1.5, delay: 0, options: [.repeat], animations: {
+            imageView.frame.origin = endPoint
+            imageView.alpha = 0
+        }, completion: { _ in
+            imageView.frame.origin.y = mainWeatherImageView.frame.maxY
+            imageView.alpha = 1
+        })
+        
+        let rotationAnimation = CABasicAnimation(keyPath: "transform.rotation")
+        rotationAnimation.fromValue = 0
+        rotationAnimation.toValue = Double.pi * 2
+        rotationAnimation.duration = 2
+        rotationAnimation.repeatCount = .infinity
+        imageView.layer.add(rotationAnimation, forKey: "rotateAnimation")
+    }
+    
+    private func sunAnimation() {
+        guard let mainWeatherImageView = mainWeatherImageView else {return}
+        
+        let rotationAnimation = CABasicAnimation(keyPath: "transform.rotation")
+        rotationAnimation.fromValue = 0
+        rotationAnimation.toValue = Double.pi * 2
+        rotationAnimation.duration = 5
+        rotationAnimation.repeatCount = .infinity
+        mainWeatherImageView.layer.add(rotationAnimation, forKey: "rotateAnimation")
+    }
     
 }
 
 extension ViewController {
     private func configureUI(){
-        configureImageView()
+        configureMainWeatherImageView()
     }
     
-    private func configureImageView() {
+    private func configureMainWeatherImageView() {
         let imageView = UIImageView()
         imageView.image = UIImage(named: "sunBig")
         
@@ -111,6 +178,7 @@ extension ViewController {
                     imageView.heightAnchor.constraint(equalToConstant: 300)
                 ])
         
-        self.imageView = imageView
+        self.mainWeatherImageView = imageView
     }
+    
 }
